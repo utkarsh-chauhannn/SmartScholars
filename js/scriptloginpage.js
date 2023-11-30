3c4ef41a24776fb535beac72e65dad15ffe44244
utkarsh-chauhannn <144721884+utkarsh-chauhannn@users.noreply.github.com>
2023-11-30 10:07:10 +0530

    Add files via upload

---
+++ js/scriptloginpage.js
@@ -0,0 +1,5 @@
+document.getElementById("Google").onclick = function () {
+    location.href = "http://127.0.0.1:3000/teacherstudent.html";
+ };
+
+ 
---
+++ js/teacandstu.js
@@ -0,0 +1,8 @@
+document.getElementById("studentchoice").onclick = function () {
+    location.href = "http://127.0.0.1:3000/home.html";
+ };
+
+ document.getElementById("teacherchoice").onclick = function () {
+    location.href = "http://127.0.0.1:3000/teacher_profile.html";
+ };
+
